5240b0e9322260254f1f3d6a0853a6f7fc95ce24
jhonnjc15 <jhonatanjcca@gmail.com>
2021-10-03 21:55:43 -0500

    Fixing pep8 errors

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """Square class"""
 
 
-class square():
+class Square():
     """Square class"""
     width = 0
     height = 0
@@ -13,10 +13,10 @@ class square():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """Perimeter"""
         return (self.width * 2) + (self.height * 2)
 
@@ -24,9 +24,9 @@ class square():
         """string"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
