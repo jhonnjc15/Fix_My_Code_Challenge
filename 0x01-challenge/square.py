d6ea7a6e6418e69ebe7606315219b71f665f8ed2
jhonnjc15 <jhonatanjcca@gmail.com>
2021-10-03 22:15:39 -0500

    Improving the doc string

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -19,15 +19,16 @@ class Square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """Perimeter"""
+        """Return the Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """string"""
+        """Dictionary representation"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
+
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
